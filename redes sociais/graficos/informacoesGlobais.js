4a4a4f6acab20ab274729b30e834dd28ba38fa6b
gustavo1506 <145714578+gustavo1506@users.noreply.github.com>
2024-10-24 15:37:29 -0300

    nnn

--- redes sociais/graficos/informacoesGlobais.js
+++ redes sociais/graficos/informacoesGlobais.js
@@ -1,14 +1,13 @@
-const url="'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'"
+const url="https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 
 async function visualizarInformaçõesGlobais() {
     const resposta = await fetch (url);
     let dados = await resposta.json();
     console.log(dados);
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container_texto')
+    const paragrafo = document.createElement("p");
+    paragrafo.classList.add("graficos-container__texto");
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${total_pessoas_conectadas}estão conectadas a alguma rede social e passam dm média ${dados.tempo_medio} horas conectadas`
-    console.log(paragrafo)
-    const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo)
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
 }
 visualizarInformaçõesGlobais();
